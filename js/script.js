1ed64e5717a698ff0e77ffcd4f74d672f22c669e
jonathan2gomes <jonathan2gomes.jg@gmail.com>
2019-11-10 22:06:35 -0300

    asdfasdf

--- js/script.js
+++ js/script.js
@@ -1,77 +1,27 @@
 function calcNum(num) {
-    if (typeof gvisor == 'undefined') {
-       document.calcform.visor.value = ''
-    }
-    document.calcform.visor.value = document.calcform.visor.value + num
-    gvisor = 1
- }
- function calcOper(oper, valor1, valor2) {
-     switch (oper){
-         case "add":
-             var valor = parseFloat(valor1) + parseFloat(valor2)
-             break
-         case "div":
-             var valor = valor1 / valor2
-             break
-        case "mult":
-            var valor = valor1 * valor2
-            break
-        case "sub":
-            var valor = valor1 - valor2
-            break   
-        case "percent":
-            var valor = valor1 % valor2
-            break
-        case "pi":
-            var valor = 3.14
-            break            
-        
-     }
-     return (valor)
- } 
-
-
-  function calcParse(oper){
-     var valor = document.calcform.visor.value 
-     delete gvisor
-
-     if (typeof goper != 'undefined' && oper == 'res'){
-         gvalor = calcOper(goper, gvalor,  valor)
-         document.calcform.visor.value = gvalor
-         delete oper
-         delete gvalor
-         return(0)
-     }
-     if (typeof gvalor != 'undefined'){
-         gvalor = calcOper(goper, gvalor, valor)
-         goper = operdocument.calcform.visor.value = gvalor
-     } else {
-         gvalor = valor
-         goper = oper
-     }
-
-
- }
+    var valor = document.calcform.visor.value;
+    document.calcform.visor.value = valor + num;
+}
  
- function clearNum() {
-    document.calcform.visor.value = ''
-    delete gvalor
-    delete goper
-    delete gvisor
- }
+function clearNum() {
+    document.calcform.visor.value = '';
+    delete num;
+}
 
-
- function parseInput(input){
-     //2 * 10% +  50 /  2
-
-     //da  esqueda para a direita opere
-
-    calculaPorcentagem(input);
-    //2 * 0.1 + 50 / 2
-
-    calculaMultDiv(input);
-    //0.2 + 25
-
-    calculaSomaSub(input);
-    //25.2
- }
+function result(){
+    var res = 0;
+    res = document.calcform.visor.value;
+    document.calcform.visor.value = '';
+    document.calcform.visor.value = eval(res);
+}
+function conversor(){
+    var res = 0;
+    res = document.calcform.visor.value;
+    document.calcform.visor.value = '';
+    document.calcform.visor.value = -1*(res);
+}
+function percent(){
+    var perc = 0;
+    perc = (num/100);
+    document.calcform.visor.value = perc;
+}
